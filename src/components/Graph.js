39cf4a566453e8068d981569b110f6c597ce80e3
emigliorata <emigliorata@toolboxtve.com>
2020-07-11 13:37:45 -0300

    Final refactoring

---
+++ src/components/Graph.js
@@ -0,0 +1,188 @@
+import React from "react";
+import CytoscapeComponent from "react-cytoscapejs";
+//import networks from "./data/networks";
+import "antd/dist/antd.css";
+//import "./index.css";
+import example2 from "./../data/example2.json";
+import example2Style from "./../data/example2-style.json";
+import example1 from "./../data/example1.json";
+import Header from "./header";
+
+class Graph extends React.Component {
+  state = {
+    layout: "concentric",
+    source: "example1",
+    fileData: null
+  };
+  shouldComponentUpdate(props, nextprops) {
+    return (
+      this.state.layout !== nextprops.layout ||
+      this.state.source !== nextprops.source
+    );
+  }
+  componentDidMount() {}
+  render() {
+    let layout = {
+      name: this.state.layout,
+      minNodeSpacing: 100,
+      directed: true
+    };
+    return (
+      <div>
+        <Header
+          setLayout={this.setLayout}
+          layout={this.state.layout}
+          handleFiles={this.handleFiles}
+          generateReport={this.generateReport}
+          fileData={this.state.fileData}
+          setReport={this.props.setReport}
+          source={this.state.source}
+          setSource={this.setSource}
+        />
+        {this.state.source === "example1" && example1 && (
+          <CytoscapeComponent
+            elements={example1}
+            style={{
+              width: "1200px",
+              height: "600px",
+              border: "solid 5px black"
+            }}
+            layout={layout}
+            cy={cy => {
+              cy.on("tap", "node", evt => {
+                var node = evt.target;
+                console.log("tapped " + node.id());
+                this.props.setMetrics({
+                  closeness: cy
+                    .$()
+                    .cc({ root: "#" + node.id(), directed: false }),
+                  betweeness: cy
+                    .$()
+                    .bc()
+                    .betweenness("#" + node.id()),
+                  degree: cy.$().dc({ root: "#" + node.id(), directed: true })
+                });
+                console.log("max", cy.$().maxDegree(false));
+              });
+              this.cy = cy;
+            }}
+          />
+        )}
+        {this.state.source === "file" && this.state.fileData && (
+          <CytoscapeComponent
+            elements={this.state.fileData}
+            style={{
+              width: "1200px",
+              height: "600px",
+              border: "solid 5px black"
+            }}
+            layout={layout}
+            cy={cy => {
+              cy.on("tap", "node", evt => {
+                var node = evt.target;
+                console.log("tapped " + node.id());
+                this.props.setMetrics({
+                  closeness: cy
+                    .$()
+                    .cc({ root: "#" + node.id(), directed: false }),
+                  betweeness: cy
+                    .$()
+                    .bc()
+                    .betweenness("#" + node.id()),
+                  degree: cy.$().dc({ root: "#" + node.id(), directed: true })
+                });
+                console.log("max", cy.$().maxDegree(false));
+              });
+              this.cy = cy;
+            }}
+          />
+        )}
+        {this.state.source === "example2" && example2 && (
+          <CytoscapeComponent
+            elements={example2}
+            stylesheet={example2Style}
+            style={{
+              width: "1200px",
+              height: "600px",
+              border: "solid 5px black"
+            }}
+            layout={layout}
+            cy={cy => {
+              cy.on("tap", "node", evt => {
+                var node = evt.target;
+                this.props.setMetrics({
+                  closeness: cy
+                    .$()
+                    .cc({ root: "#" + node.id(), directed: false }),
+                  betweeness: cy
+                    .$()
+                    .bc()
+                    .betweenness("#" + node.id()),
+                  degree: cy.$().dc({ root: "#" + node.id(), directed: true })
+                });
+              });
+              this.cy = cy;
+            }}
+          />
+        )}
+      </div>
+    );
+  }
+
+  generateReport = () => {
+    let result = "";
+    let cy = this.cy;
+    cy.filter(function(element, i) {
+      if (element.isNode()) {
+        const degree = cy.$().dc({ root: "#" + element.id(), directed: true });
+        const closeness = cy
+          .$()
+          .cc({ root: "#" + element.id(), directed: false })
+          .toFixed(2);
+        const betweeness = cy
+          .$()
+          .bc()
+          .betweenness("#" + element.id())
+          .toFixed(2);
+        result +=
+          `Nodo ${element.id()}: ` +
+          `Closeness: ${closeness} - Betweeness: ${betweeness} - ` +
+          `Indegree: ${degree.indegree} - Outdegree: ${degree.outdegree} \n`;
+
+        return true;
+      }
+      return false;
+    });
+    setTimeout(() => {
+      window.scrollTo(0, document.body.scrollHeight);
+    }, 500);
+    this.props.setReport(result);
+  };
+
+  handleFiles = files => {
+    let reader = new FileReader();
+    reader.onload = e => {
+      this.setState({ fileData: JSON.parse(e.target.result), source: "file" });
+    };
+    reader.readAsText(files[0]);
+    this.props.setReport(null)
+  };
+
+  setLayout = value => {
+    this.cy
+      .layout({
+        name: value,
+        minNodeSpacing: 100,
+        directed: true
+      })
+      .run();
+    this.setState({ layout: value });
+  };
+  
+  setSource = value => {
+    this.setState({ source: value });
+    this.props.setReport(null);
+  };
+}
+
+export default Graph;
---
+++ src/components/header.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Typography, Select, Button } from "antd";
+import ReactFileReader from "react-file-reader";
+const { Text } = Typography;
+const { Option } = Select;
+
+const Header = ({
+  setLayout,
+  layout,
+  setReport,
+  handleFiles,
+  fileData,
+  generateReport,
+  source,
+  setSource
+}) => {
+  return (
+    <div style={{ display: "flex" }}>
+      <div>
+        <Text>Seleccione layout de grafo</Text>
+        <Select
+          style={{ width: 120, margin: 10 }}
+          value={layout}
+          onChange={value => setLayout(value)}
+        >
+          <Option value={"random"}>Random</Option>
+          <Option value={"cose"}>Compound Spring Embedder</Option>
+          <Option value={"breadthfirst"}>Breadthfirs</Option>
+          <Option value={"circle"}>Circle</Option>
+          <Option value={"concentric"}>Concentric</Option>
+        </Select>
+      </div>
+      <div>
+        <Text>Seleccione Source del grafo</Text>
+        <Select
+          style={{ width: 220, margin: 10 }}
+          value={source}
+          onChange={value => setSource(value)}
+        >
+          <Option value={"example1"}>Ejemplo 1</Option>
+          <Option value={"example2"}>Ejemplo 2 </Option>
+          {fileData && <Option value={"file"}>Archivo </Option>}
+        </Select>
+      </div>
+      <div style={{ margin: "auto", display: "flex" }}>
+        <Button onClick={generateReport}>Generar Reporte</Button>
+        <ReactFileReader
+          fileTypes={[".json"]}
+          base64={false}
+          multipleFiles={false}
+          handleFiles={handleFiles}
+        >
+          <Button> Cargar desde archivo</Button>
+        </ReactFileReader>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
